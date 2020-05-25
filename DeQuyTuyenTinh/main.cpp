70dda866bd1b7eef8d01e9338cfc99ad52545153
huynhthehung <huynhthehung96@gmail.com>
2020-05-25 22:54:11 +0700

    Add project files.

---
+++ DeQuyTuyenTinh/main.cpp
@@ -0,0 +1,32 @@
+// DeQuyTuyenTinh.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int factorial(int n)
+{
+	if (n == 0)
+		return 1;
+	return n * factorial(n - 1);
+}
+int main()
+{
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+	int kq = factorial(n);
+	cout << n << "!= " << kq;
+	return 0;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
